83ebc711e22d47c1f10e21e5a6cfb60983196b40
Marina <pchupchu@yandex.ru>
2024-11-21 12:17:08 +0300

    Add basic styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,19 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        accent: '#591ddd',
+        accent_secondary: '#8728e1',
+      },
+      backgroundImage: {
+        body: "url('/body-bg.png')",
       },
     },
   },
