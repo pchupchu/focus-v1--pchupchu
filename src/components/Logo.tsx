ada308fd26e9efb0972154a391c184519133ed2b
Marina <pchupchu@yandex.ru>
2024-11-24 11:29:36 +0300

    Fix logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,14 +3,8 @@ import Link from 'next/link';
 
 const Logo = () => {
   return (
-    <Link className='relative' href='/'>
-      <Image
-        className='object-contain'
-        src='/logo.svg'
-        alt='logo'
-        width={148}
-        height={22}
-      />
+    <Link href='/'>
+      <Image src='/logo.svg' alt='logo' width={148} height={22} />
     </Link>
   );
 };
